928e8be758fa454c9979e1c1571dc11f97ac41c1
dave <devscrapper@yahoo.fr>
2017-07-26 19:09:05 +0200

    V0.6 Ajout d'un service de recherche  qui s'appuie sur les moteur de recherche google, bing, yahoo.
    v0.51
    ajout de la date de telechargement
    
    v0.5
    
    ajout du server de license : il permet de telecharge une licence pour chaque machine qui heberge statupbot
    
    v0.4
    
    ajout d'un service de recuperation des parametres http d'une requete pour évaluer le comportement d'un proxy
    
    v0.3
    
    rename de visitor_id en visit_id, afin de pouvoir envoyer les captcha vers statupweb qui ne connais pas le visitor.
    de toute façon visitor_idou visit_id cela n'a pas d'importance pour saas rails car cela est utilisé pour l'unicité de la demande
    
    v0.2
    
    mise en place de decaptcher pour convertir les captcha en text.

--- db/migrate/20170726165929_change_properties_to_text_in_search.rb
+++ db/migrate/20170726165929_change_properties_to_text_in_search.rb
@@ -1,5 +1,6 @@
 class ChangePropertiesToTextInSearch < ActiveRecord::Migration
   def change
+    change_column_default(:searches, :results, nil)
     change_column :searches, :results, :text
   end
 end
--- db/migrate/20170726170440_suppres_default_value_results_in_search.rb
+++
@@ -1,5 +0,0 @@
-class SuppresDefaultValueResultsInSearch < ActiveRecord::Migration
-  def change
-    change_column_default(:searches, :results, nil)
-  end
-end
